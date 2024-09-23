0818bde4edc2ca65f148ceec454dcd3ed5d7c748
Victor Padula <victorhpadula@gmail.com>
2024-09-23 18:21:11 -0300

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,165 +1,22 @@
-import React from "react";
+import { Curriculum, NavBar } from "@/components";
 
-const CVPage: React.FC = () => {
+export default function Home() {
     return (
-        <div className="text-justify">
-            <div className="flex flex-col justify-center items-center mb-1">
-                <h1 className="text-3xl">Victor Padula</h1>
-                <h1 className="text-2xl">Software Engineer</h1>
-            </div>
-            <div className="flex flex-col justify-center items-center mb-2">
-                <p>São Paulo - SP - Brazil</p>
-                <p>
-                    Email:{" "}
-                    <a href="mailto:victorhpadula@gmail.com">
-                        victorhpadula@gmail.com
-                    </a>{" "}
-                    | Phone:{" "}
-                    <a href="tel:+5515991462145">+55 (15) 99146-2145</a>
-                </p>
-                <p>
-                    Links: <a href="https://linktr.ee/vhpadula">Portfolio</a> |{" "}
-                    <a href="https://linkedin.com/in/vhpadula">LinkedIn</a> |{" "}
-                    <a href="https://github.com/vhpadula">GitHub</a>
-                </p>
-            </div>
-            <div className="m-2">
-                <h2 className="text-xl">Education</h2>
-                <ul className=" mx-1 list-disc list-inside">
-                    <li>
-                        University of Sao Paulo, Bachelor in Computer
-                        Engineering (2019 – 2024)
-                    </li>
-                    <li>
-                        Politecnico di Torino, Master’s Degree Erasmus in
-                        Software Engineering (2022 - 2023)
-                    </li>
-                </ul>
-            </div>
-            <div className="m-2">
-                <h2 className="text-xl">Experience</h2>
-                <ul className="mx-1 list-disc list-inside">
-                    <li>
-                        <strong>XR & Data Engineer Intern, Virtualisurg</strong>{" "}
-                        - Paris, Montreal and Sao Paulo (May 2024 - Sep 2024)
-                        <ul>
-                            <li>
-                                Developed internal tools and PoC projects for XR
-                                experiences applied to health and medicine using
-                                Unity Engine
-                            </li>
-                            <li>
-                                Worked on backend infrastructure for license
-                                control and data collection
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>
-                            Fullstack Software Developer, REAG Investimentos
-                        </strong>{" "}
-                        - Sao Paulo, SP (Sep 2023 – Dec 2023)
-                        <ul>
-                            <li>
-                                Developed internal tools for data analysis and
-                                connection to finance market APIs
-                            </li>
-                            <li>
-                                Designed UX and Cloud Architecture with Django
-                                framework
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>Data Scientist Intern</strong> - Sao Paulo, SP
-                        (Jan 2022 – Apr 2022)
-                        <ul>
-                            <li>
-                                Worked in data engineering, experimenting with
-                                AI models
-                            </li>
-                            <li>
-                                Developed data analytics modules with GCP and
-                                machine learning in Python and SQL
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>Mobile Developer Intern</strong> - Sao Paulo, SP
-                        (May 2021 - Aug 2021)
-                        <ul>
-                            <li>
-                                Developed Android mobile applications with
-                                Kotlin in Android Studio
-                            </li>
-                            <li>
-                                Worked on production-scale systems in a Scrum
-                                team
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <div className="m-2">
-                <h2 className="text-xl">Projects</h2>
-                <ul className=" mx-1 list-disc list-inside">
-                    <li>
-                        <strong>ONGAdmin (Graduation Thesis)</strong> (2024)
-                        <ul>
-                            <li>
-                                Developed UX and system architecture for
-                                open-source NGO administration tool
-                            </li>
-                            <li>Tools: Next.js, Spring Boot</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>Vardis Transfer System</strong> (2024)
-                        <ul>
-                            <li>
-                                Developed a real-time money transfer system for
-                                board game players
-                            </li>
-                            <li>Tools: Next.js, Node.js, Redis</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <strong>JingasAI</strong> (2024)
-                        <ul>
-                            <li>
-                                AI project implementing a Brazilian Portuguese
-                                &quot;Alexa&quot; with NLP models
-                            </li>
-                            <li>Tools: Pytorch, Docker</li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <div className="m-2">
-                <h2 className="text-xl">Skills</h2>
-                <ul className=" mx-1 list-disc list-inside">
-                    <li>
-                        <strong>Languages:</strong> Python, Java, Kotlin, C#,
-                        SQL, HTML, CSS, JavaScript, TypeScript
-                    </li>
-                    <li>
-                        <strong>Technologies:</strong> Django, Pandas, Pytorch,
-                        Spring Boot, Android Studio, React, Next.js, Node.js,
-                        Unity3D, Figma, Docker, Linux, GCP
-                    </li>
-                    <li>
-                        <strong>Proficiencies:</strong> Software Engineering,
-                        Cloud Computing, System Architecture, Machine Learning,
-                        International Team Communication
-                    </li>
-                    <li>
-                        <strong>Idioms: </strong>Native Portuguese, English C1,
-                        Italian A2
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
-};
+        <>
+            <div className="min-h-full">
+                <NavBar />
 
-export default CVPage;
+                <header className="bg-white shadow">
+                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                        <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+                            About Me
+                        </h1>
+                    </div>
+                </header>
+                <main>
+                    <Curriculum />
+                </main>
+            </div>
+        </>
+    );
+}
---
+++ src/components/organisms/NavBar.tsx
@@ -0,0 +1,89 @@
+import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+    { name: "About Me", href: "/", current: true },
+    { name: "Engineering", href: "/Engineering", current: false },
+    { name: "Photography", href: "/Photography", current: false },
+    { name: "Music", href: "/Music", current: false },
+    { name: "Publications Feed", href: "/Publications", current: false },
+];
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(" ");
+}
+
+export default function NavBar() {
+    return (
+        <Disclosure as="nav" className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 items-center justify-between">
+                    <div className="flex items-center">
+                        <div className="hidden md:block">
+                            <div className="ml-10 flex items-baseline space-x-4">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        aria-current={
+                                            item.current ? "page" : undefined
+                                        }
+                                        className={classNames(
+                                            item.current
+                                                ? "bg-gray-900 text-white"
+                                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                            "rounded-md px-3 py-2 text-sm font-medium"
+                                        )}
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="-mr-2 flex md:hidden">
+                        {/* Mobile menu button */}
+                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                            <span className="absolute -inset-0.5" />
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon
+                                aria-hidden="true"
+                                className="block h-6 w-6 group-data-[open]:hidden"
+                            />
+                            <XMarkIcon
+                                aria-hidden="true"
+                                className="hidden h-6 w-6 group-data-[open]:block"
+                            />
+                        </DisclosureButton>
+                    </div>
+                </div>
+            </div>
+
+            <DisclosurePanel className="md:hidden">
+                <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                    {navigation.map((item) => (
+                        <DisclosureButton
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            aria-current={item.current ? "page" : undefined}
+                            className={classNames(
+                                item.current
+                                    ? "bg-gray-900 text-white"
+                                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                "block rounded-md px-3 py-2 text-base font-medium"
+                            )}
+                        >
+                            {item.name}
+                        </DisclosureButton>
+                    ))}
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
+    );
+}
